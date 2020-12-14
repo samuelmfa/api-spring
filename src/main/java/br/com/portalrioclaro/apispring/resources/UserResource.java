16d40aa705fe9b2b35c7faca62eb7369708b4d86
Samuel Costa <samuelmfa@gmail.com>
2020-12-14 07:38:00 -0300

    Update User

--- src/main/java/br/com/portalrioclaro/apispring/resources/UserResource.java
+++ src/main/java/br/com/portalrioclaro/apispring/resources/UserResource.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,7 +17,6 @@ import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import br.com.portalrioclaro.apispring.entities.User;
 import br.com.portalrioclaro.apispring.services.UserService;
-import net.bytebuddy.asm.Advice.OffsetMapping.ForOrigin.Renderer.ForReturnTypeName;
 
 @RestController
 @RequestMapping(value = "/users")
@@ -49,6 +49,12 @@ public class UserResource {
 	public ResponseEntity<Void> delete(@PathVariable Long id) {
 		service.delete(id);
 		return ResponseEntity.noContent().build();
+	}
+
+	@PutMapping(value = "/{id}")
+	public ResponseEntity<User> update(@PathVariable Long id, @RequestBody User obj) {
+		obj = service.update(id, obj);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
